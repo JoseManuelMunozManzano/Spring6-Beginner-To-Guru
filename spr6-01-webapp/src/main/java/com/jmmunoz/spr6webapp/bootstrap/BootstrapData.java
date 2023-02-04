816964a793a55569b2b699e30fcd73458525dad7
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-02-04 06:42:31 +0100

    Publisher: Entidad, repository e inicialización con datos

--- spr6-01-webapp/src/main/java/com/jmmunoz/spr6webapp/bootstrap/BootstrapData.java
+++ spr6-01-webapp/src/main/java/com/jmmunoz/spr6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.jmmunoz.spr6webapp.bootstrap;
 
 import com.jmmunoz.spr6webapp.domain.Author;
 import com.jmmunoz.spr6webapp.domain.Book;
+import com.jmmunoz.spr6webapp.domain.Publisher;
 import com.jmmunoz.spr6webapp.repositories.AuthorRepository;
 import com.jmmunoz.spr6webapp.repositories.BookRepository;
+import com.jmmunoz.spr6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,16 @@ public class BootstrapData implements CommandLineRunner {
     // Es una buena práctica declarar componentes del contexto Spring como final, para que no puedan cambiar
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     // Al ejecutarse, Spring hace el autowire automático de las implementaciones de los repositorios que nos
     // proporciona Spring Data JPA
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -63,5 +69,24 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+
+        // Informando editores
+        Publisher addison = new Publisher();
+        addison.setPublisherName("Addison Wesley");
+        addison.setAddress("123 Street");
+        addison.setCity("Reading");
+        addison.setState("Massachusets");
+        addison.setZipCode("12345");
+        publisherRepository.save(addison);
+
+        Publisher wiley = new Publisher();
+        wiley.setPublisherName("Addison Wesley");
+        wiley.setAddress("111 River Street Hoboken");
+        wiley.setCity("New York");
+        wiley.setState("New York");
+        wiley.setZipCode("07030");
+        publisherRepository.save(wiley);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
---
+++ spr6-01-webapp/src/main/java/com/jmmunoz/spr6webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.jmmunoz.spr6webapp.repositories;
+
+import com.jmmunoz.spr6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
