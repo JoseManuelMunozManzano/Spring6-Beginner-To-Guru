1f48b9e5da7690d08faecf48ee286ed330e1e651
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-02-04 05:18:47 +0100

    Métodos toString(), equals() y hashCode()

--- spr6-01-webapp/src/main/java/com/jmmunoz/spr6webapp/domain/Author.java
+++ spr6-01-webapp/src/main/java/com/jmmunoz/spr6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.jmmunoz.spr6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -48,5 +49,35 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    // Siempre es bueno crear el método toString()
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    // Es importante crear los métodos equals() y hashCode() porque Hibernate los usa de forma interna para determinar
+    // la igualdad entre objetos.
+    // Hay un par de estrategias que se pueden implementar, como utilizar solo el campo Id o todos los campos de la entidad.
+    // En este ejemplo se va a usar solo el campo Id
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
